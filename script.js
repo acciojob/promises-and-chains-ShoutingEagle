b9de08d3dc5643893beb6a62431d2dc100a240d4
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-08-03 21:54:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded' ,function () {
+	const form = document.getElementById('formData');
+
+
+	form.addEventListener('submit',function (event){
+		event.preventDefault();
+		const age = document.getElementById("age").value;
+		const name = document.getElementById("name").value;
+		console.log(age);
+		console.log(name);
+		const promise = new Promise((resolve,reject) => {
+			setTimeout (() => {
+				if(age>= 18){
+					resolve(`Welcome, ${name}. You can vote.`);
+				}
+				else{
+					reject(`Oh sorry ${name}. You aren't old enough.`);
+				}
+			},4000)
+		})
+		promise.then((message) => {
+			alert(message);
+		})
+		.catch((error) => {
+			alert(error);
+		})
+		
+	})
+	
+})
+// 
